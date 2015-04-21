ccff2054b42cf0228349e963b5f11c0b06014f66
Steven <theepicsnail@gmail.com>
2015-04-20 22:42:00 -0700

    tell plugin

---
+++ plugins/tell.py
@@ -0,0 +1,34 @@
+import api
+import datetime
+from collections import defaultdict
+messages = defaultdict(list)
+
+"""
+Minor interesting point. if you tell yourself a message, then the
+order of api.onPrivmsg and api.onCommand fire matters.
+
+As of the time of this writing, they are executed in order of
+decorator application. activity calls its decorator before onTell
+so privmsg is handled, then onTell.
+
+Meaning you can send yourself a !tell, that wont be repeated until
+your next message sent somewhere the bot can see.
+"""
+
+@api.onPrivmsg()
+def activity(who, what, where):
+    if who in messages:
+        for msg in messages[who]:
+            api.privmsg(who, msg)
+        del messages[who]
+
+@api.onCommand("tell")
+def onTell(who, args, where):
+    nick, msg = args.split(" ", 1)
+    when = datetime.datetime.now().strftime("%H:%M:%S %m/%d/%y")
+
+    msg = when + " " + nick +": " + msg
+    messages[nick].append(msg)
+
+
+
