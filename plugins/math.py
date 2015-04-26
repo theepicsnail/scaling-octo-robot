4fe0b13d96c97f44cb4dd6b5387f762351a67b50
Steven <theepicsnail@gmail.com>
2015-04-26 11:01:32 -0700

    Fixed up a lot of the math plugin

--- plugins/math.py
+++ plugins/math.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-DEBUG = False
+DEBUG = True
 from collections import namedtuple
 import re
 sm = staticmethod
@@ -24,7 +24,7 @@ if not DEBUG:
 class TokenStream:
   def __init__(self, line):
     self.pos = 0
-    self.toks = list(filter(bool,re.split(r"(\d+\.\d+|\d+|\w+)", line)))
+    self.toks = list(filter(bool,re.split(r"(\d+\.\d+|\d+|\w+|.)", line)))
     self.toks.append("<END>")
 
   def get(self):
@@ -39,6 +39,7 @@ class TokenStream:
     return str(self.pos) + str(self.toks)
   def __repr__(self):
     return str(self)
+
 def optional(cls, toks):
   t = toks.pos
   val = cls.parse(toks)
@@ -76,9 +77,11 @@ class VariableAssignment(namedtuple('VariableAssignment', ['name', 'expr'])):
       return None
     return VariableAssignment(name, expr)
 
-  def evaluate(self):
-    scope[self.name] = self.expr.evaluate()
+  def evaluate(self, scope):
+    scope[self.name] = self.expr.evaluate(scope)
     return scope[self.name]
+
+  def __str__(self): return "{}={}".format(self.name, self.expr)
 
 class FunctionalAssignment(namedtuple('FunctionalAssignment', ['name', 'args', 'expr'])):
   @staticmethod
@@ -105,10 +108,46 @@ class FunctionalAssignment(namedtuple('FunctionalAssignment', ['name', 'args', '
         if arg ==',':
           arg = line.get()
 
-    expr = Assignment(line)
+    if line.get() != "=":
+        return None
+
+    expr = Assignment.parse(line)
     if expr is None:
       return None
-    return FunctionAssignment(name, args, expr)
+    return FunctionalAssignment(name, args, expr)
+
+  def evaluate(self, scope):
+    scope[self.name] = self
+    return Atom(None, self.name).evaluate(scope)
+
+
+  def call(self, args):
+    print("Call", args, self)
+    if len(args) != len(self.args):
+        return None
+    nscope = scope.copy()
+    nscope.update(dict(zip(self.args, [
+            n.evaluate(scope) for n in args])))
+    return self.expr.evaluate(nscope)
+
+  def __str__(self):
+    return "{}({})={}".format(self.name, ",".join(self.args), self.expr)
+
+class NativeFunction:
+  def __init__(self, name, func):
+    self.name = name;
+    self.func = func
+  def evaluate(self, scope):
+    scope[self.name] = self
+    return self
+
+  def call(self, args):
+    print("NativeCall", args, self)
+    vals = [e.evaluate(scope) for e in args]
+    return self.func(*vals)
+
+  def __str__(self):
+    return self.name
 
 
 class Expression(namedtuple('Expression', ['head', 'tail'])):
@@ -133,10 +172,10 @@ class Expression(namedtuple('Expression', ['head', 'tail'])):
       return head
     return Expression(head, tail)
 
-  def evaluate(self):
-    val = self.head.evaluate()
+  def evaluate(self, scope):
+    val = self.head.evaluate(scope)
     for (mod, term) in self.tail:
-      v = term.evaluate()
+      v = term.evaluate(scope)
       if mod == '+':
         val += v
       elif mod == '-':
@@ -145,6 +184,10 @@ class Expression(namedtuple('Expression', ['head', 'tail'])):
         raise("error")
     return val
 
+  def __str__(self):
+    return "{}{}".format(self.head, "".join([
+      "{}{}".format(*t) for t in self.tail
+    ]))
 
 class Term(namedtuple('Term', ['head', 'tail'])):
   @staticmethod
@@ -156,6 +199,7 @@ class Term(namedtuple('Term', ['head', 'tail'])):
     tail = []
     while True:
       op = line.get()
+      print("op", op)
       if op not in "*/%":
         line.unget()
         break
@@ -167,10 +211,10 @@ class Term(namedtuple('Term', ['head', 'tail'])):
       return Term(head, tail)
     return head
 
-  def evaluate(self):
-    val = self.head.evaluate()
+  def evaluate(self, scope):
+    val = self.head.evaluate(scope)
     for (mod, term) in self.tail:
-      v = term.evaluate()
+      v = term.evaluate(scope)
       if mod == '*':
         val *= v
       elif mod == '/':
@@ -180,6 +224,11 @@ class Term(namedtuple('Term', ['head', 'tail'])):
       else:
         raise("error")
     return val
+
+  def __str__(self):
+    return "{}{}".format(self.head, "".join([
+      "{}{}".format(*t) for t in self.tail
+    ]))
 
 class Factor(namedtuple('Factor', ['base', 'exp'])):
   @staticmethod
@@ -197,10 +246,10 @@ class Factor(namedtuple('Factor', ['base', 'exp'])):
     return Factor(head, exp)
 
 
-  def evaluate(self):
-    return self.base.evaluate() ** self.exp.evaluate()
+  def evaluate(self, scope):
+    return self.base.evaluate(scope) ** self.exp.evaluate(scope)
 
-class Value(namedtuple('Value', ['base', 'mod'])):
+class Value(namedtuple('Value', ['base', 'mod', 'tail'])):
   @staticmethod
   def parse(line):
     mod = line.get()
@@ -210,20 +259,78 @@ class Value(namedtuple('Value', ['base', 'mod'])):
     base = Atom.parse(line)
     if base is None:
       return None
-    if mod is None:
-      return base
-    return Value(base, mod)
 
-  def evaluate(self):
-    val = self.base.evaluate()
+    tail = []
+    while True:
+        t = optional(Tail, line)
+        if t is None:
+            break
+        tail.append(t)
+
+    if not mod and not tail:
+      return base
+    return Value(base, mod, tail)
+
+  def evaluate(self, scope):
+    val = self.base.evaluate(scope)
+    for t in self.tail:
+        print(t, val)
+        val = t.evaluate(scope,val)
+
     if self.mod == '-':
       val *= -1
     return val
+
+  def __str__(self):
+    out = []
+    if self.mod:
+        out.append("-")
+    out.append(str(self.base))
+    out.extend(self.tail)
+    return "".join([str(x) for x in out])
+
+class Tail(namedtuple('Tail', ['args'])):
+    @staticmethod
+    def parse(line):
+        val = line.get()
+        if val != '(':
+            line.unget()
+            return None
+        args = []
+        while True:
+            val = optional(Expression, line)
+            if val is None:
+                return None
+
+            args.append(val)
+            val = line.get()
+
+            if val == ')':
+                return Tail(args)
+            elif val != ',':
+                return None
+
+    def evaluate(self, scope, func):
+        if type(func) in [FunctionalAssignment, NativeFunction]:
+            # self.args contains the expression trees for the arguments
+            return func.call(self.args)
+        return None
+
+    def __str__(self):
+        return "({})".format(",".join([str(x) for x in self.args]))
 
 class Atom(namedtuple('Atom', ['value', 'name'])):
   @staticmethod
   def parse(line):
     tok = line.get()
+    if tok == '(': # subexpressions
+        val = Expression.parse(line)
+        if val is None:
+            return None
+        tok = line.get()
+        if tok != ')':
+            return None
+        return val
     if tok.isalpha():
       return Atom(None, tok)
     try:
@@ -231,7 +338,10 @@ class Atom(namedtuple('Atom', ['value', 'name'])):
     except:
       return None
 
-  def evaluate(self):
+  def __str__(self):
+      return str(self.value) if self.value is not None else self.name
+
+  def evaluate(self, scope):
     if self.value is not None:
       return self.value
     return scope.get(self.name, 0)
@@ -240,9 +350,20 @@ def evaluate(line):
   tree = Assignment.parse(TokenStream(line.replace(" ","")))
   if tree is None:
       return None
-  return tree.evaluate()
+  global scope
+  return tree.evaluate(scope)
 
-scope = {}
+import math
+scope = {
+  "sin": NativeFunction("sin", math.sin)
+}
+for name in dir(math):
+  attr = getattr(math, name)
+  if callable(attr):
+    scope[name] = NativeFunction(name, attr)
+  elif type(attr) == float:
+    scope[name] = attr
+    print(name, type(attr))
 
 
 import api
@@ -254,6 +375,6 @@ def math(sender, message, target):
   equ = message[1:].replace(" ","")
   value = evaluate(equ)
   if value is not None:
-      api.privmsg(target, "{GREEN}" + str(evaluate(equ)))
+      api.privmsg(target, "{GREEN}" + str(value))
 
 
