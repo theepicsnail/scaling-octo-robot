df9b1b70f040b0298e4c3a069a2bb912d25b9198
Steven <theepicsnail@gmail.com>
2015-04-26 15:59:45 -0700

    Fxied html escaping

--- plugins/titles.py
+++ plugins/titles.py
@@ -1,13 +1,15 @@
 import api
 import re
 import urllib.request
+import html
 
 url_re = "(https?[^\\s]+)"
 
 def getTitle(url):
-    html = urllib.request.urlopen(url).read().decode("utf-8")
-    html = " ".join(html.split("<title>")[1].split("</title>")[0].split())
-    return html
+    data = urllib.request.urlopen(url).read().decode("utf-8")
+    title = " ".join(data.split("<title>")[1].split("</title>")[0].split())
+    return html.unescape(title)
+
 @api.onPrivmsg()
 def handle(who, msg, where):
     parts = re.split(url_re, msg)
