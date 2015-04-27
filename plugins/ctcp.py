65061dae24625bf2de45e834301f8cab627fb06e
Steven <theepicsnail@gmail.com>
2015-04-26 21:51:12 -0700

    Ctcp fixes

--- plugins/ctcp.py
+++ plugins/ctcp.py
@@ -1,10 +1,13 @@
 import api
 import time
+import subprocess
+
+version = subprocess.check_output(["git", "show", "-s", "--format=%h"]).decode('utf-8', 'ignore').strip()
 
 @api.onCTCP("VERSION")
-def version(who, cmd, args):
-  api.ctcp(who,"VERSION", "SomeIrcBot 1.2.3")
+def onVersion(who, cmd, args):
+  api.ctcp(who,"VERSION", "scaling-octo-robot " + version)
 
 @api.onCTCP("TIME")
-def version(who, cmd, args):
+def onTime(who, cmd, args):
   api.ctcp(who, "TIME", time.ctime())
