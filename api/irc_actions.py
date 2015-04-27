aa4dbca692f38951ca131fbf1f592b2596b3fa88
Steven <theepicsnail@gmail.com>
2015-04-26 21:42:16 -0700

    Adding more actions

--- api/irc_actions.py
+++ api/irc_actions.py
@@ -5,12 +5,18 @@ def raw(line):
 def rawf(line, *args):
   raw(line.format(*args))
 
+def notice(target, message):
+  rawf(u"NOTICE {} :{}", target, message)
+
 def msg(target, message):
   rawf(u"PRIVMSG {} :{}", target, message)
 privmsg = msg
 
 def join(chan):
-  rawf("JOIN {}", chan)
+  rawf(u"JOIN {}", chan)
 
+def ctcp(who, cmd, val):
+  if val:
+    cmd += " " + val
 
-
+  notice(who, u"\x01{}\x01".format(cmd))
