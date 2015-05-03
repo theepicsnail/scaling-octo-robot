7525352a3fde6fa6fc8d6182e226d4a5882a2632
Steven <theepicsnail@gmail.com>
2015-05-03 13:26:56 -0700

    Testing stdout

--- plugins/eval_test.py
+++ plugins/eval_test.py
@@ -1,19 +1,41 @@
 from testing.testcase import TestCase
+from unittest.mock import patch
+from io import StringIO
 from . import eval
 
 # Make the commands blocking.
 eval.bg = lambda cmd,*args:cmd(*args)
 
+
 class TestEval(TestCase):
-  def test_echo(self):
+
+  @patch('sys.stdout', new_callable=StringIO)
+  def test_echo(self, stdout):
     self.alice.say("!eval echo test")
     self.alice.assertMsg(eval.stdoutFmt.format("'test'"))
 
-  def test_bad_lang(self):
+    #TODO fix this assertion
+    self.assertNotEqual(
+        stdout.getvalue(),
+        "")
+
+  @patch('sys.stdout', new_callable=StringIO)
+  def test_bad_lang(self, stdout):
     self.alice.say("!eval ../bad.sh muahaha")
     self.alice.assertMsg(eval.usage)
 
-  def test_no_bash_evaluation(self):
+    #TODO fix this assertion
+    self.assertNotEqual(
+        stdout.getvalue(),
+        "")
+
+  @patch('sys.stdout', new_callable=StringIO)
+  def test_no_bash_evaluation(self, stdout):
     self.alice.say("!eval echo *")
     self.alice.assertMsg(eval.stdoutFmt.format("'*'"))
     # Should be '*' not a directory list.
+
+    #TODO fix this assertion
+    self.assertNotEqual(
+        stdout.getvalue(),
+        "")
